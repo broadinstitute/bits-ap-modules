32ef2fc272d9bfc67cadc34a1c80fd6644410b42
Henry Ferrara <ferrara@broadinstitute.org>
2021-06-14 12:46:59 -0400

    update trigger for more variables and allow map of substitutions

--- tf-modules/cloudbuild_trigger/variables.tf
+++ tf-modules/cloudbuild_trigger/variables.tf
@@ -2,47 +2,39 @@ variable "branch_name" {
   description = "The name of the github branch."
   type        = string
 }
-variable "container_name" {
-  description = "The names used for a repo container image."
-  default     = null
-  type        = string
-}
-variable "container_tag" {
-  description = "The tag used for a repo container image."
-  default     = null
-  type        = string
-}
-variable "cloudbuild_dir_path" {
-  description = "The path for the directory for cloudbuild."
-  default     = null
-  type        = string
-}
 variable "description" {
   description = "The description of the trigger."
   type        = string
 }
-variable "dns_zone" {
-  description = "The dns zone."
+variable "filename" {
+  description = "The location of the cloudbuild yaml."
+  default     = "cloudbuild.yaml"
+  type        = string
+}
+variable "github_owner" {
+  description = "Github owner that repo is part of"
+  default     = "broadinstitute"
+  type        = string
+}
+variable "github_repo" {
+  description = "Github repo trigger is against"
   default     = null
   type        = string
 }
-variable "filename" {
-  description = "The location of the cloudbuild yaml."
-  type        = string
+variable "ignored_files" {
+  description = "List of files/regex that the trigger will ignore changes on"
+  type        = list(any)
+  default     = null
 }
 variable "included_files" {
   description = "The files/folder that the trigger watches."
-  type        = string
-}
-variable "repo_path" {
-  description = "The path to repo."
+  type        = list(any)
   default     = null
-  type        = string
 }
-variable "state_bucket" {
-  description = "The terraform state bucket."
+variable "substitutions" {
+  description = "Map of substitutions used by trigger"
   default     = null
-  type        = string
+  type        = map(any)
 }
 variable "trigger_name" {
   description = "The name of the trigger."
